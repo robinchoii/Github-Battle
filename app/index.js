663b1e2205f543426ceff0b41e36f5f73c5e1ab7
Robin Choi <robinchoii@gmail.com>
2017-04-20 18:48:35 -0700

    setup webpack babel react

---
+++ app/index.js
@@ -0,0 +1,17 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+require('./index.css');
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>hello world!</div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
