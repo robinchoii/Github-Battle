dede50d83831b082d84cdfdd57017dacc1173403
Robin Choi <robinchoii@gmail.com>
2017-04-24 15:43:53 -0700

    add nav with router to popular

--- app/components/App.js
+++ app/components/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import Popular from './Popular'
+import Popular from './Popular';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Nav from './Nav';
+
 
 class App extends React.Component {
-
   render() {
     return (
-      <div className='container'>
-        <Popular />
-      </div>
+      <Router>
+        <div className='container'>
+          <Nav/>
+          <Route path='/popular' component={Popular} />
+        </div>
+      </Router>
     )
   }
 }
