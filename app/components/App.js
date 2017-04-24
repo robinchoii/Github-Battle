3c64ac4123dbe8f58b65c85ec9baf582a8d60fc5
Robin Choi <robinchoii@gmail.com>
2017-04-24 16:05:58 -0700

    add home and battle component, skeleton only

--- app/components/App.js
+++ app/components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Popular from './Popular';
 import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Nav from './Nav';
+import Home from './Home';
+import Battle from './Battle';
 
 
 class App extends React.Component {
@@ -10,6 +12,8 @@ class App extends React.Component {
       <Router>
         <div className='container'>
           <Nav/>
+          <Route exact path='/' component={Home} />
+          <Route path='/battle' component={Battle} />
           <Route path='/popular' component={Popular} />
         </div>
       </Router>
